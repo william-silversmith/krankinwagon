454274434c32b54cdf1527bfa343e0fd34db2862
mvanlonden <melvanlonden@gmail.com>
2014-09-06 22:54:31 -0400

    too much monster

--- public/scripts/controllers/main.js
+++ public/scripts/controllers/main.js
@@ -8,25 +8,44 @@
  * Controller of the krankinwagonApp
  */
 angular.module('krankinwagonApp')
-  .controller('MainCtrl', function ($scope, angSocket) {
+  .controller('MainCtrl', function ($scope, angSocket, $interval) {
     $scope.command = {};
     $scope.health = 100;
+    $scope.timeLeft = 1;
+
+    angSocket.forward('command');
     $scope.$on('socket:command', function (ev, data){
       $scope.command = data;
+      $scope.timeLeft = 100;
     });
+
     $scope.toggleControl = function (id) {
       var controlEvent = {
         id: id,
         value: true
-      }
+      };
       angSocket.emit('control', controlEvent);
     }
+
+    angSocket.forward('lifecycle');
     $scope.$on('socket:lifecycle', function (ev, data) {
       $scope.lifecycle = data;
+      if (data == 'start') {
+        $scope.health = 100;
+      }
     });
+
+    $interval(function () {
+      if ($scope.timeLeft > 0) {
+          $scope.timeLeft = $scope.timeLeft - 1;
+      }
+    }, 1000);
+
     $scope.stopSession = function() {
       angSocket.emit('lifecycle', 'stop');
     }
+
+    angSocket.forward('health');
     $scope.$on('socket:health', function (ev, data) {
       $scope.health = data;
     });
